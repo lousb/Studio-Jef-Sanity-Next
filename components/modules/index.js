47b5d345329241e796150add089d4c419ebcaa01
lousb <wyethtafe@gmail.com>
2025-06-23 13:31:31 +1000

    ongoing changes — sanity mux integration

--- components/modules/index.js
+++ components/modules/index.js
@@ -4,6 +4,7 @@ const SingleImage = dynamic(() => import('./single-image'))
 const TwoImages = dynamic(() => import('./two-images'))
 const TextBlock = dynamic(() => import('./text-box'))
 const SingleVideo = dynamic(() => import('./single-video'))
+const SingleVideoMux = dynamic(() => import('./single-video-mux'))
 const TwoVideos = dynamic(() => import('./two-videos'))
 
 export const Module = ({
@@ -20,6 +21,8 @@ export const Module = ({
         return <TextBlock data={content} />
     case 'singleVideo':
       return <SingleVideo data={content} />
+    case 'videoBlogPost':
+      return <SingleVideoMux data={content} />
     case 'twoVideos':
       return <TwoVideos data={content} />
     default:
