fa94a9a684192498881cc98d0cff2db6818b86ea
lousb <wyethtafe@gmail.com>
2026-08-07 14:54:13 +1000

    intial setup and layout

--- app/api/sync-featured-media/route.ts
+++ app/api/sync-featured-media/route.ts
@@ -6,7 +6,7 @@ const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2024-01-01',
-  token: process.env.SANITY_WRITE_TOKEN,
+  token: process.env.SANITY_API_WRITE_TOKEN,
   useCdn: false,
 })
 
